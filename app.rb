dd1fb06d05df5c531f3dd813ce6670c30e19f980
Maya Alkhabaz <mayalkhabaz@gmail.com>
2026-09-18 22:14:00 -0700

    Finished CHIPS

--- app.rb
+++ app.rb
@@ -41,6 +41,14 @@ class WordGuesserApp < Sinatra::Base
   post '/guess' do
     params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      valid = @game.guess(params[:guess].to_s[0])
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
+
+    flash[:message] = "You have already used that letter." if valid == false
+
     redirect '/show'
   end
 
@@ -51,16 +59,22 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    status = @game.check_win_or_lose
+    redirect '/win' if status == :win
+    redirect '/lose' if status == :lose
+
     erb :show # You may change/remove this line
   end
 
   get '/win' do
     ### YOUR CODE HERE ###
+    redirect '/show' unless @game.check_win_or_lose == :win
     erb :win # You may change/remove this line
   end
 
   get '/lose' do
     ### YOUR CODE HERE ###
+    redirect '/show' unless @game.check_win_or_lose == :lose
     erb :lose # You may change/remove this line
   end
 end
--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,11 +1,57 @@
 class WordGuesserGame
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
+  MAX_WRONG_GUESSES = 7
 
   # Get a word from remote "random word" service
+  attr_reader :word
 
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @correct_letters = []
+    @incorrect_letters = []
+  end
+
+  def guess(letter)
+    unless letter.is_a?(String) && letter.length == 1 && letter =~ /[a-zA-Z]/
+      raise ArgumentError, "guess must be a single letter"
+    end
+
+    letter = letter.downcase
+    already_tried = @correct_letters.include?(letter) || @incorrect_letters.include?(letter)
+    return false if already_tried
+
+    if @word.include?(letter)
+      @correct_letters << letter
+    else
+      @incorrect_letters << letter
+    end
+  end
+
+  def guesses
+    @correct_letters.join
+  end
+
+  def wrong_guesses
+    @incorrect_letters.join
+  end
+
+  def word_with_guesses
+    result = ""
+    @word.each_char do |ch|
+      result << (@correct_letters.include?(ch) ? ch : "-")
+    end
+    result
+  end
+
+  def check_win_or_lose
+    if word_with_guesses == @word
+      :win
+    elsif @incorrect_letters.length >= MAX_WRONG_GUESSES
+      :lose
+    else
+      :play
+    end
   end
 
   # You can test it by installing irb via $ gem install irb
